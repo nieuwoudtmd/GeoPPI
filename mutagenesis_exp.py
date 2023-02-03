917f3257991540327f10c26aacb5ab00d87d48e3
Mechiel <mechiel@silicogenesis.com>
2023-02-03 17:00:52 +0200

    script update

--- mutagenesis_exp.py
+++ mutagenesis_exp.py
@@ -224,7 +224,7 @@ def mutagenesis_experiment():
     epitope_list = ["epitope_original", "epitope_top_1", "epitope_top_2", "epitope_diff_1", "epitope_diff_2",
                     "epitope_diff_3"]
 
-    for complex in complex_list:
+    for complex in complex_list[1:]:
         project_dir = os.path.join(os.getcwd(), "data", f"mutagenesis_experiment_{complex}")
 
         for i, experiment in enumerate(epitope_list):
