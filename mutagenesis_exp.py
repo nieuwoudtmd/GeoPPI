c8d55034083fb6aa714faddfc04ea925e2339c9c
Mechiel <mechiel@silicogenesis.com>
2023-03-03 15:12:28 +0200

    script update

--- mutagenesis_exp.py
+++ mutagenesis_exp.py
@@ -240,7 +240,7 @@ def mutagenesis_experiment():
     for complex in complex_list:
         project_dir = os.path.join(os.getcwd(), "data", "mutagenesis_experiments", complex)
 
-        for i, experiment in enumerate(epitope_list[-1:]):
+        for i, experiment in enumerate(epitope_list):
             time_start = time.time()
             print(f"Running job({i + 1}/{len(epitope_list)}): {experiment} ")
             mutagenesis_exp = Mutagenesis(project_dir, experiment)
